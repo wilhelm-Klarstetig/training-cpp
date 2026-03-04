450aae0999aca54ce4dbc801788b0b2706629517
wilhelm-Klarstetig <cenkaijunwilhelm@outlook.com>
2026-03-04 16:25:20 +0800

    feat: finish exercises 00-02

--- exercises/01_variable&add/main.cpp
+++ exercises/01_variable&add/main.cpp
@@ -3,6 +3,8 @@
 // READ: 运算符 <https://zh.cppreference.com/w/cpp/language/expressions#.E8.BF.90.E7.AE.97.E7.AC.A6>
 
 int main(int argc, char **argv) {
+    int x ;
+    std::cin>>x;
     // TODO: 补全变量定义并打印加法运算
     // x ?
     std::cout << x << " + " << x << " = " << x + x << std::endl;
--- exercises/02_function/main.cpp
+++ exercises/02_function/main.cpp
@@ -1,4 +1,5 @@
 #include "../exercise.h"
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -8,46 +9,64 @@
 
 // TODO: 在这里声明函数
 // 基本函数声明练习
+int add(int a, int b);
 
 // 函数重载练习
+double add(double a, double b);
+std::string add(const std::string& a, const std::string& b);
 
 // 数组操作函数练习
+int sum_array(const std::vector<int>& arr);
 
 // 字符串处理函数练习
 std::string reverse_string(const std::string& str);
 
 int main(int argc, char **argv) {
     // 基本函数练习
-    ASSERT(add(123, 456) == ?, "add(123, 456) should be ?");
-    auto x = 1, y = 2;
+    ASSERT(add(123, 456) == 579, "add(123, 456) should be 579");
+    auto x{1}, y{2};
     std::cout << x << " + " << y << " = " << add(x, y) << std::endl;
 
     // 函数重载练习
-    ASSERT(add(3.14, 2.86) == ?, "add(3.14, 2.86) should be ?");
-    ASSERT(add(std::string("Hello, "), std::string("World!")) == ?, "add(\"Hello, \", \"World!\") should be ?");
+    ASSERT(add(3.14, 2.86) == 6.0, "add(3.14, 2.86) should be 6.0");
+    // 使用双括号包裹，防止宏将 std::string 内部的逗号误判为宏参数分隔符
+    ASSERT((add(std::string{"Hello, "}, std::string{"World!"}) == "Hello, World!"), "add(\"Hello, \", \"World!\") should be \"Hello, World!\"");
 
     // 数组操作练习
-    std::vector<int> numbers = {1, 2, 3, 4, 5};
-    ASSERT(sum_array(numbers) == ?, "sum_array({1, 2, 3, 4, 5}) should be ?");
+    std::vector<int> numbers{1, 2, 3, 4, 5};
+    ASSERT(sum_array(numbers) == 15, "sum_array({1, 2, 3, 4, 5}) should be 15");
 
     // 字符串处理练习
-    ASSERT(reverse_string("hello") == ?, "reverse_string(\"hello\") should be ?");
+    ASSERT(reverse_string("hello") == "olleh", "reverse_string(\"hello\") should be \"olleh\"");
 
     return 0;
 }
 
 // TODO: 补全函数定义，但不要移动代码行
 int add(int a, int b) {
+    return a + b;
 }
 
 double add(double a, double b) {
+    return a + b;
 }
 
 std::string add(const std::string& a, const std::string& b) {
+    return a + b;
 }
 
 int sum_array(const std::vector<int>& arr) {
+    int sum{0};
+    for (int n : arr) {
+        sum += n;
+    }
+    return sum;
 }
 
 std::string reverse_string(const std::string& str) {
+    std::string res{""};
+    for (int i=str.length() - 1; i >= 0; --i) {
+        res += str[i];
+    }
+    return res;
 }
