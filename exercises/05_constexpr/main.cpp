75bdbfa8752635e76b9084bb8dae00dd16b69656
wilhelm-Klarstetig <cenkaijunwilhelm@outlook.com>
2026-03-06 00:08:14 +0800

    ex05

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -1,14 +1,27 @@
 #include "../exercise.h"
 
 constexpr unsigned long long fibonacci(int i) {
-    switch (i) {
-        case 0:
-            return 0;
-        case 1:
-            return 1;
-        default:
-            return fibonacci(i - 1) + fibonacci(i - 2);
+   // 基础情况处理
+    if (i == 0) {
+        return 0;
     }
+    if (i == 1) {
+        return 1;
+    }
+
+    // 使用迭代法，只记录前两项的值
+    unsigned long long a{0};
+    unsigned long long b{1};
+    unsigned long long result{0};
+
+    // 从第 2 项开始循环累加
+    for (int j{2}; j <= i; ++j) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+
+    return result;
 }
 
 int main(int argc, char **argv) {
@@ -19,7 +32,7 @@ int main(int argc, char **argv) {
     // TODO: 观察错误信息，修改一处，使代码编译运行
     // PS: 编译运行，但是不一定能算出结果……
     constexpr auto ANS_N = 90;
-    constexpr auto ANS = fibonacci(ANS_N);
+    auto ANS = fibonacci(ANS_N);
     std::cout << "fibonacci(" << ANS_N << ") = " << ANS << std::endl;
 
     return 0;
